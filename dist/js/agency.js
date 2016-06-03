b8a62f42ebcbeda7ebbc5a4c680bbb72e8a7de27
elsehow <yes@cosmopol.is>
2016-06-03 00:39:21 -0700

    disabled broken scrollspy in js/agency.js, temporarily removing the highlight until things are fixed

--- dist/js/agency.js
+++ dist/js/agency.js
@@ -9,16 +9,16 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top-50
+            scrollTop: $($anchor.attr('href')).offset().top-65
         }, 700, 'easeInOutExpo');
         event.preventDefault();
     });
 });
 
 // Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+// $('body').scrollspy({
+//     target: '.navbar-fixed-top'
+// })
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
