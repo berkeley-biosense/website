8ae05ec355058190c1ef96e9b0d2527dab8ad6a8
elsehow <yes@cosmopol.is>
2016-06-03 11:23:31 -0700

    updated john's fixes, added prod deploy npm script

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ module.exports = {
   people: [
     {
       name: 'John Chuang',
-      title: 'Co-Director',
+      title: 'Director',
       img: 'people-photos-small/john2.jpg',
     },
     {
@@ -90,7 +90,7 @@ module.exports = {
       img: 'people-photos-small/jen.jpg',
     },
     {
-      name: 'Max Curarn',
+      name: 'Max Curran',
       title: 'PhD Student',
       img: 'people-photos-small/max.jpg',
     },
